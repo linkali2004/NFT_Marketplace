2b494b9466b169fafa0284a569f12599deeddb64
linkali2004 <22je0919@iitism.ac.in>
2024-01-15 04:23:54 +0530

    Second Commit

--- backend/app.js
+++ backend/app.js
@@ -2,33 +2,38 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const port=  3000;
-
+const mongoose = require("mongoose");
 
 app.use(express.json());
 const result = JSON.parse(fs.readFileSync("./data/nfts.json"));
-app.get("/api/v1/nfts/",(req,res)=>{
-    res.status(200).json({
-        data:result
-    })
-})
 
+let connection = false;
+let nft=null;
 const nftLengthValidator = (req,res,next)=>
 {
-    const id = req.params.id;
-    if(id > result.length)
+    if(connection)
     {
-        return res.status(404).json({
-            data:"Data not found"
-        })
+        const id = req.params.id;
+        if(id > result.length)
+        {
+            return res.status(404).json({
+                data:"Data not found"
+            })
+        }
+        next();
     }
-    next();
+    else
+    {
+        res.send("Please wait while the connection is made");
+    }
 }
 
-app.route("/api/v1/nfts/:id").get(nftLengthValidator,(req,res)=>{
-    const id = req.params.id;
-    const nft = result.find((el)=>el.id==id);
+app.route("/api/v1/nfts/:id").get(nftLengthValidator, async (req,res)=>{
+    const idtosearch = Number(req.params.id);
+    const result = await nft.findOne({id:idtosearch});
+    console.log(result);
     res.status(200).json({
-        data:nft
+        data:result
     })
 }).patch(nftLengthValidator,(req,res)=>{
     res.status(200).json({
@@ -40,7 +45,32 @@ app.route("/api/v1/nfts/:id").get(nftLengthValidator,(req,res)=>{
     })
 })
 
+app.route("/api/v1/nfts").get( async (req,res)=>{
+    const result = await nft.find(req.query).select({name:1,duration:1}).sort({duration:-1});
 
-app.listen(port,()=>{
+    res.status(200).json({
+        data:result
+    })
+})
+
+const nftSchema = new mongoose.Schema({
+    id:{type:Number,required:true,min:0},
+    name:{type:String,required:true},
+    duration:{type:Number},
+    maxGroupSize:Number,
+    difficulty:String,
+    ratingsAverage:{type:Number,default:3.1},
+    ratingsQuantity:Number,
+    price:Number,
+    summary:String,
+    description:String,
+    imageCover:String,
+    images:[String],
+    startDates:[String]
+})
+
+app.listen(port,async ()=>{
+    connection= await mongoose.connect("mongodb+srv://shrey2004:shrey@cluster0.9j4ff.mongodb.net/nfts?retryWrites=true&w=majority");
+    nft=mongoose.model("Nfts",nftSchema);
     console.log("App running on port 3000");
 })
